339c85813a830cd930b0085c53b6b012a736d0e9
Maminka1948 <138146832+maminka19482@users.noreply.github.com>
2025-04-10 20:54:36 +0200

    Innit upload

---
+++ Callouts/talahons.cs
@@ -0,0 +1,45 @@
+﻿using LSPD_First_Response.Engine;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Germancallouts.Callouts
+{
+    [CalloutInfo("Talahonfight", CalloutProbability.High)]
+    
+    class Talahonfight : Callout
+    {
+        private Ped _Talahon1;
+        private Ped _Talahon2;
+
+        private Vector3 _Spawnpoint;
+
+        private Blip _sceneblip;
+
+        private Ped Player = Game.LocalPlayer.Character;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            _Spawnpoint =  World.GetNextPositionOnStreet(Player.Position.Around(100f));
+
+            ShowCalloutAreaBlipBeforeAccepting(_Spawnpoint, 30f);
+            CalloutMessage = "Talahonfight";
+            CalloutPosition = _Spawnpoint;
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+        public override bool OnCalloutAccepted()
+        {
+
+            return base.OnCalloutAccepted();
+        }
+        private void Crtalahon()
+        {
+            _Talahon1 = 
+        }
+    }
+}
